c100f622f3eaaf7231dae28bb52bf668fde0e882
nelle-au-miel <jlatortue@proton.me>
2026-09-20 14:05:31 -0400

    Initial Commit

---
+++ SwiftSpace/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftSpace
+//
+//  Created by Jaynelle Latortue-Raymond (Étudiant) on 2026-09-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftSpace/SwiftSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSpaceApp.swift
+//  SwiftSpace
+//
+//  Created by Jaynelle Latortue-Raymond (Étudiant) on 2026-09-20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
